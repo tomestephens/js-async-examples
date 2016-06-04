9610d3a68fdc8809a4e1355a780368fa3bfe5b5c
Tom Stephens <tostephens@fanatics.com>
2016-06-04 14:03:10 -0600

    Initial commit. Examples of bluebird promises, async/await w/promises & callbacks.

---
+++ asyncawaitlib.js
@@ -0,0 +1,65 @@
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs')),
+      async = require('asyncawait/async'),
+      await = require('asyncawait/await');
+
+console.log('Start...');
+
+console.time('process');
+console.time('read file');
+
+const run = async (() => {
+    var buffer = await (fs.readFileAsync('test-data.csv'));
+    var text = await (bufferToText(buffer));
+    var csv = await (textToCsv(text));
+
+    // change a cell
+    csv[1][1] = 'new value';
+    // add a new row
+    csv.push(['new row-col1','new row-col2','new row-col3']);
+
+    buffer = await (csvToBuffer(csv));
+
+    console.time('write file');
+    await (fs.writeFileAsync('test-data-asyncawaitlib.csv', buffer));
+    console.log("Save complete.");
+    console.timeEnd('write file');
+    console.timeEnd('process');
+});
+
+run();
+
+const bufferToText = async (buf => {
+    console.time('buffer to text');
+    let text = '';
+    for(let c of buf) {
+        text+= String.fromCharCode(c);
+    }
+    console.timeEnd('buffer to text');
+    return text;
+});
+
+const textToCsv = async ((text) => {
+    console.time('text to csv');
+    const lines = text.split('\n'),
+        csv = [];
+
+    for(let line of lines) {
+        csv.push(line.split(','));
+    }
+
+    console.timeEnd('text to csv');
+    return csv;
+});
+
+const csvToBuffer = async ((csvData) => {
+    console.time('csv to buffer');
+
+    let text = '';
+    for(let line of csvData) {
+        text += `${line.join(',')}\n`;
+    }
+
+    console.timeEnd('csv to buffer');
+    return text;
+});
---
+++ bluebird.js
@@ -0,0 +1,78 @@
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+
+console.log('Start...');
+console.time('process');
+console.time('read file');
+fs.readFileAsync('test-data.csv').then(buffer => {
+        console.timeEnd('read file');
+        return bufferToText(buffer);
+    }).then(textToCsv).then(csv => {
+        // change a cell
+        csv[1][1] = 'new value';
+        // add a new row
+        csv.push(['new row-col1','new row-col2','new row-col3']);
+
+        return csvToBuffer(csv);
+    }).then(buffer => {
+        console.time('write file');
+        return fs.writeFileAsync('test-data-bluebird.csv', buffer);
+    }).then(() => {
+        console.log('Save complete.');
+        console.timeEnd('write file');
+        console.timeEnd('process');
+    }).catch(err => console.error(err));
+
+
+function bufferToText(buf) {
+    return new Promise((resolve, reject) => {
+        try {
+            console.time('buffer to text');
+            let text = '';
+            for(let c of buf) {
+                text+= String.fromCharCode(c);
+            }
+            console.timeEnd('buffer to text');
+            resolve(text);
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+
+function textToCsv(text) {
+    return new Promise((resolve, reject) => {
+        try {
+            console.time('text to csv');
+            const lines = text.split('\n'),
+                csv = [];
+
+            for(let line of lines) {
+                csv.push(line.split(','));
+            }
+
+            console.timeEnd('text to csv');
+            resolve(csv);
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+
+function csvToBuffer(csvData) {
+    return new Promise((resolve, reject) => {
+        try {
+            console.time('csv to buffer');
+
+            let text = '';
+            for(let line of csvData) {
+                text += `${line.join(',')}\n`;
+            }
+
+            console.timeEnd('csv to buffer');
+            resolve(Buffer.from(text));
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
